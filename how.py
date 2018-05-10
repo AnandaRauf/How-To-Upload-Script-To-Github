0e86892760d6cef9c1285093e1002fdcace42fce
AnandaRauf <uzumaki.nanda@rocketmail.com>
2018-05-10 21:44:23 +0700

    versi 1.0.0

---
+++ how.py
@@ -0,0 +1,15 @@
+"""
+How To Upload Script To Github
+10 Mei 2018
+Ananda Rauf Maududi
+Code Phyton The Best
+"""
+how1= " git user.name 'your username github ' "
+how2= " git user.email your email github "
+how3= " git init "
+how4= " git add * "
+how5= " git commit -m "your version commit whatever you" "
+how6= " git remote add origin your repository url "
+how7= " git pull origin  "
+how8= "git push -u origin master "
+print how1, how2, how3, how4, how5, how6, how7, how8
